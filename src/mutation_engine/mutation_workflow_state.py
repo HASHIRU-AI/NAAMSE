8fe9605e87e0373e2bd2cd5bd819d041cafcf833
Parth Shah <helloparthshah@gmail.com>
2025-11-08 16:40:23 -0800

    Created the mutation framework

--- src/mutation_engine/mutation_workflow_state.py
+++ src/mutation_engine/mutation_workflow_state.py
@@ -1,4 +1,9 @@
+from enum import Enum
 from typing_extensions import List, Literal, TypedDict, Optional
+
+
+class Mutation(Enum):
+    SUBSTITUTION = "substitution"
 
 
 class BasePrompt(TypedDict):
@@ -10,15 +15,18 @@ class ScoredPrompt(BasePrompt):
     """Prompt with associated score."""
     score: float
 
+
 class MutatedPrompt(BasePrompt):
     """Prompt with associated score."""
-    mutation_type: Literal["paraphrase", "add_negation", "synonym_swap", "split_prompt"]
+    mutation_type: Mutation
+
 
 class MutationWorkflowState(TypedDict):
     """State for the mutation subgraph."""
     prompt_to_mutate: BasePrompt
     mutated_prompt: BasePrompt | MutatedPrompt
-    mutation_type: Literal["paraphrase", "add_negation", "synonym_swap", "split_prompt"]
+    mutation_type: Literal["paraphrase",
+                           "add_negation", "synonym_swap", "split_prompt"]
 
 
 class MutationEngineState(TypedDict):
@@ -26,12 +34,12 @@ class MutationEngineState(TypedDict):
     # Inputs
     input_prompts: List[ScoredPrompt]
     n_to_generate: int
-    
+
     # Internal state
     prompt_probabilities: List[float]
     selected_prompt: Optional[ScoredPrompt]
     action_to_take: Literal["mutate", "similar", "explore"]
     newly_generated_prompt: Optional[BasePrompt]
-    
+
     # Output
     final_generated_prompts: List[BasePrompt]
