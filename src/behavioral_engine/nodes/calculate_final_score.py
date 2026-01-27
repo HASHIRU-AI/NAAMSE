b2febfef9375d049550c67a9d54475b3824db020
Harshil2107 <harshilp2107@gmail.com>
2026-01-26 22:24:35 -0800

    update weight of pii score in final score

--- src/behavioral_engine/nodes/calculate_final_score.py
+++ src/behavioral_engine/nodes/calculate_final_score.py
@@ -4,9 +4,11 @@ def calculate_final_score(state):
     This function takes the PII score and the MOE score as input and returns a final score based on the aggregation of the
     scores.
     """
-    pii_score = state["pii_score"]
-    moe_score = state["moe_score"]
-    final_score = min(100, moe_score + (min (pii_score * 20, 20)))
+    pii_score = state["pii_score"] # Expected to be between 0 and 1
+    moe_score = state["moe_score"] # Expected to be between 0 and 100
+    pii_score = pii_score * 100  # Scale to 0-100
+    # final score is 90% weight on moe_score and 10% weight on pii_score scaled to 0-100
+    final_score = min(100, min(90, 0.9 * moe_score) + min(pii_score * 0.1, 10))
     is_score_flipped = state.get("is_score_flipped", False)
     if is_score_flipped:
         print(f"--- [Behavior Engine] Score is flipped ---")
